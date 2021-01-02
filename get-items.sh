23b0e4d8470b86d4bc645f6c4254d88f12ed1ec4
Frank Otto <traktofon@fastmail.com>
2021-01-02 17:03:20 +0000

    add some items manually

--- get-items.sh
+++ get-items.sh
@@ -4,6 +4,11 @@ mkdir -p 'items'
 
 while read slug; do
    [ -f "items/$slug.html" ] && continue
-   wget --quiet "https://taustation.space/item/$slug" -O "items/$slug.html" && sleep 0.5
-   echo $slug
+   if wget --quiet "https://taustation.space/item/$slug" -O "items/$slug.html"; then
+      sleep 0.5
+      echo $slug
+   else
+      rm -f "items/$slug.html"
+      echo "FAILED: $slug" >&2
+   fi
 done
